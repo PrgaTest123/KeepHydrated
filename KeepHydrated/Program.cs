581402dcd9ed6d219228a324312f4981a90cad05
PrgaTest123 <marintest0@gmail.com>
2022-09-29 21:13:30 +0200

    Add project files.

---
+++ KeepHydrated/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+public class Function 
+{
+    //every 1h of cycling drink 0.5l of water
+    public static int Liters(double time) => (int)(time * 0.5);
+   
+
+}
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Input how much time have youe spent: ");
+        double time = Double.Parse(Console.ReadLine());
+        Console.WriteLine("You have dranked " + Function.Liters(time) + " liters of water");
+    }
+    }
+
